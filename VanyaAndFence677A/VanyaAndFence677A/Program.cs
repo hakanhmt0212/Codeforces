1a22d0e3a5d1d1d365612d8437a7171161d69649
Ahmet Hakan Demirel <05190000043@ogrenci.ege.edu.tr>
2021-11-17 11:41:30 +0300

    677A

---
+++ VanyaAndFence677A/VanyaAndFence677A/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace VanyaAndFence677A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1 = Console.ReadLine();
+            string heightofPerson = Console.ReadLine();
+
+            string[] vs = input1.Split(" ");
+            int friends = Convert.ToInt32(vs[0]);
+            int heightFence = Convert.ToInt32(vs[1]);
+
+            Console.WriteLine(width(friends, heightFence, heightofPerson));
+        }
+
+        public static int width(int friends, int wallHeight, string personHeight)
+        {
+            int result = friends;
+            string[] array = personHeight.Split(" ");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int height = Convert.ToInt32(array[i]);
+
+                if (height > wallHeight)
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+    }
+}
